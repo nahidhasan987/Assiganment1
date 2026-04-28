13ca9303b2be844f085424cdc642e263603c58c4
nahidhasan987 <siayubali257@gmail.com>
2026-04-29 00:25:39 +0600

    q4.dart

---
+++ Lib/q4.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+
+void main() {
+  print("Enter principal:");
+  double p = double.parse(stdin.readLineSync()!);
+
+  print("Enter time:");
+  double t = double.parse(stdin.readLineSync()!);
+
+  print("Enter rate:");
+  double r = double.parse(stdin.readLineSync()!);
+
+  double si = (p * t * r) / 100;
+  print("Simple Interest = $si");
+}
